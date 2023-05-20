d5a2cd821bf0bd7d994f9fb53faa72a0b71a2ac1
masud-rana44 <mdmasudrana4488@gmail.com>
2023-05-20 23:15:34 +0600

    Initial commit

---
+++ client/src/components/FormFild.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormFild = () => {
+  return <div>FormFild</div>;
+};
+
+export default FormFild;
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import Card from "./Card";
+import FormFild from "./FormFild";
+import Loader from "./Loader";
+
+export { Card, FormFild, Loader };
